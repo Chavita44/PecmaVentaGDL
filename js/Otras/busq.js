d5f4d44fcc8d43cb95213db46371510e76fe364c
Chava Garcia <garciagomezchava61@gmail.com>
2025-11-27 11:41:50 -0600

    fix bugs de rutas

--- js/Otras/busq.js
+++ js/Otras/busq.js
@@ -4,96 +4,96 @@
         tipo:"terrenos",
         nombre: "Terreno en Zapopan",
         descripcion: "500m² en zona residencial con servicios.",
-        imagen: "/Imagenes/terrenos/Halo1.png", // imagen local en tu carpeta
-        enlace: "/HTML/terrenos/terreno1.html"
+        imagen: "../../Imagenes/terrenos/Halo1.png", // imagen local en tu carpeta
+        enlace: "../../HTML/terrenos/terreno1.html"
     },
     {
         tipo: "terreno",
         tipo:"terrenos",
         nombre: "Terreno en Tala",
         descripcion: "800m² en zona residencial con servicios.",
-        imagen: "/Imagenes/terrenos/Tala.png", // imagen local en tu carpeta
-        enlace: "/HTML/terrenos/terreno2.html"
+        imagen: "../../Imagenes/terrenos/Tala.png", // imagen local en tu carpeta
+        enlace: "../../HTML/terrenos/terreno2.html"
     },
     {
         tipo: "terreno",
         tipo:"terrenos",
         nombre: "Terreno en Colima",
         descripcion: "700m² en zona residencial con servicios.",
-        imagen: "/Imagenes/terrenos/Halo3.png", // imagen local en tu carpeta
-        enlace: "/HTML/terrenos/terreno3.html"
+        imagen: "../../Imagenes/terrenos/Halo3.png", // imagen local en tu carpeta
+        enlace: "../../HTML/terrenos/terreno3.html"
     },
     {
         tipo: "terreno",
         tipo:"terrenos",
         nombre: "Terreno en Chapala",
         descripcion: "600m² en zona residencial con servicios.",
-        imagen: "/Imagenes/terrenos/Halo4.png", // imagen local en tu carpeta
-        enlace: "/HTML/terrenos/terreno4.html"
+        imagen: "../../Imagenes/terrenos/Halo4.png", // imagen local en tu carpeta
+        enlace: "../../HTML/terrenos/terreno4.html"
     },
     {
         tipo: "bodega",
         tipo:"bodegas",
         nombre: "Bodega Industrial",
         descripcion: "1200m² con acceso para tráiler.",
-        imagen: "/Imagenes/bodegas/Goku.png",
-        enlace: "/HTML/bodegas/bodega1.html"
+        imagen: "../../Imagenes/bodegas/Goku.png",
+        enlace: "../../HTML/bodegas/bodega1.html"
     },
     {
         tipo: "bodega",
         tipo:"bodegas",
         nombre: "Bodega Refugio",
         descripcion: "1200m² con acceso para tráiler.",
-        imagen: "/Imagenes/bodegas/Goku2.png",
-        enlace: "/HTML/bodegas/bodega12html"
+        imagen: "../../Imagenes/bodegas/Goku2.png",
+        enlace: "../../HTML/bodegas/bodega12html"
     },
     {
         tipo: "bodega",
         tipo:"bodegas",
         nombre: "Bodega MX",
         descripcion: "1200m² con acceso para tráiler.",
-        imagen: "/Imagenes/bodegas/Goku3.png",
-        enlace: "/HTML/bodegas/bodega3.html"
+        imagen: "../../Imagenes/bodegas/Goku3.png",
+        enlace: "../../HTML/bodegas/bodega3.html"
     },
     {
         tipo: "bodega",
         tipo:"bodegas",
         nombre: "Bodega Ruiseñores",
         descripcion: "1200m² con acceso para tráiler.",
-        imagen: "/Imagenes/bodegas/Goku4.png",
-        enlace: "/HTML/bodegas/bodega4.html"
+        imagen: "../../Imagenes/bodegas/Goku4.png",
+        enlace: "../../HTML/bodegas/bodega4.html"
     },
     {
         tipo: "otro",
         tipo:"otros",
         nombre: "Local Comercial",
         descripcion: "Espacio de 300m² ideal para tienda.",
-        imagen: "/Imagenes/otros/Fre1.png",
-        enlace: "/HTML/otros/otros1.html"
+        imagen: "../../Imagenes/otros/Fre1.png",
+        enlace: "../../HTML/otros/otros1.html"
     },
     {
         tipo: "otro",
         tipo:"otros",
         nombre: "Local centro",
         descripcion: "Espacio de 300m² ideal para tienda.",
-        imagen: "/Imagenes/otros/Fre2.png",
-        enlace: "/HTML/otros/otros2.html"
+        imagen: "../../Imagenes/otros/Fre2.png",
+        enlace: "../../HTML/otros/otros2.html"
     },
     {
         tipo: "otro",
         tipo:"otros",
         nombre: "Local piso 2",
         descripcion: "Espacio de 300m² ideal para tienda.",
-        imagen: "/Imagenes/otros/Fre3.png",
-        enlace: "/HTML/otros/otros3.html"
+        imagen: "../../Imagenes/otros/Fre3.png",
+        enlace: "../../HTML/otros/otros3.html"
     },
     {
         tipo: "otro",
         tipo:"otros",
         nombre: "Local No Comercial",
         descripcion: "Espacio de 300m² ideal para tienda.",
-        imagen: "/Imagenes/otros/Fre4.png",
-        enlace: "/HTML/otros/otros4.html"
+        imagen: "../../Imagenes/otros/Fre4.png",
+        enlace: "../../HTML/otros/otros4.html"
     }
     ];
 
